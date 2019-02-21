47f13ba29a6f5f4a33f01e9ec06879c22e754242
yahwang <ts.yahwang@gmail.com>
2019-02-21 19:00:01 +0900

    1467 풀이

--- BaekJoon/Quiz1476.java
+++ BaekJoon/Quiz1476.java
@@ -8,11 +8,6 @@ public class Main{
         s = sc.nextInt();
         m = sc.nextInt();
         sc.close();
-
-        // s로 만든 결과값을 e, m으로 각각 뺀 다음 나누어 떨어지는 지 확인
-        // e.g.) 7 15 13 => 28*4+15 
-        // ((28*4+15)-7) / 15 == 0
-        // ((28*4+15)-13) / 19 == 0 
         while(true){
             res = 28*cnt+s;
             if((res-e)%15 == 0 && (res-m)%19 == 0){
@@ -20,7 +15,6 @@ public class Main{
             }
             cnt++;
         }
-
         System.out.println(res);
     }
 }
