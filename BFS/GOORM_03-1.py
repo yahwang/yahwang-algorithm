d7d1d342f2f388765bf16956c8542f0d9664c686
yahwang <ts.yahwang@gmail.com>
2020-03-11 01:39:03 +0900

    goorm 비타알고 문제풀이 추가

---
+++ BFS/GOORM_03-1.py
@@ -0,0 +1,55 @@
+# 비타알고 시즌3 03월 1주차 백신문제
+
+import sys
+from collections import deque
+
+# 문제에 맞게 개수 조절
+adj = [[] for _ in range(200001)]
+# set으로 중복 체크
+ck = set()
+
+
+def bfs(p):
+    # 이미 확인한 환자는 제외해서 불필요 연산 제거
+    if p in ck:
+        return 0
+
+    queue = deque()
+    cnt = 0
+    queue.append(p)
+
+    # 무한루프에 빠지지 않도록 ck값으로 확인
+    while len(queue) != 0:
+        p_next = queue.popleft()
+        if p_next not in ck:
+            ck.add(p_next)
+            cnt += 1
+            for i in adj[p_next]:
+                queue.append(i)
+    return cnt
+
+
+def solution():
+    input = sys.stdin.readline
+
+    N, M = [int(i) for i in input().split()]
+    for _ in range(M):
+        p1, p2 = list(map(int, input().split()))
+        adj[p1].append(p2)
+        adj[p2].append(p1)
+
+    res = 0
+    res_p = 0
+
+    for patient in range(1, N + 1):
+        total = bfs(patient)
+        if total > res:
+            res = total
+            res_p = patient
+
+    print(res_p, res)
+
+
+if __name__ == "__main__":
+    solution()
+
--- README.md
+++ README.md
@@ -1,5 +1,15 @@
 # yahwang-algorithm
 
-### [백준 알고리즘](https://www.acmicpc.net/user/yahwang)
+## USER INFO
 
-### [CODESIGNAL](https://app.codesignal.com/profile/yahwang/overview)
+### [백준 알고리즘 - yahwang](https://www.acmicpc.net/user/yahwang)
+
+### [CODESIGNAL - yahwang](https://app.codesignal.com/profile/yahwang/overview)
+
+## VS code 세팅
+
+input.txt를 읽어서 자동으로 output으로 print 결과를 내보내줌
+
+화면처럼 args 설정을 추가해주면 된다. **Ctrl + F5**로 실행
+
+![vscode_setting](./vscode_setting.png)
